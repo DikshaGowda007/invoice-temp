a647c1d339a5f878360e111391224ca5412a2ef3
Diksha Gowda <dikshagowda2@gmail.com>
2026-09-19 12:23:26 +0530

    chore(telescope): add Laravel Telescope for local debugging

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -3,9 +3,16 @@
 use App\Providers\AppServiceProvider;
 use App\Providers\RepositoryServiceProvider;
 use App\Providers\RouteServiceProvider;
+use App\Providers\TelescopeServiceProvider;
 
-return [
+$providers = [
     AppServiceProvider::class,
     RepositoryServiceProvider::class,
     RouteServiceProvider::class,
 ];
+
+if (env('APP_ENV') === 'local') {
+    $providers[] = TelescopeServiceProvider::class;
+}
+
+return $providers;
